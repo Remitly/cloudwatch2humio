7cbd29f99761c094e2fc61f626f2b8ca91e82d9c
Caleb McCombs <calebm@remitly.com>
2021-07-30 18:09:51 -0700

    Adjust Subscriber To Look For Tags
    Setup the subscription lambda to search for tags (among other changes
    and bug fixes). I'll enumerate later but it's already 6pm on a Friday so
    I'm out for now.

--- src/logs_subscriber.py
+++ src/logs_subscriber.py
@@ -5,6 +5,7 @@ import helpers
 # Set environment variables.
 humio_log_ingester_arn = os.environ["humio_log_ingester_arn"]
 humio_subscription_prefix = os.environ.get("humio_subscription_prefix")
+humio_subscription_tag_name = os.environ.get("humio_susbscription_tag_name")
 
 # Set up CloudWatch Logs client.
 log_client = boto3.client("logs")
@@ -24,16 +25,26 @@ def lambda_handler(event, context):
     """
     # Grab the log group name from incoming event.
     log_group_name = event["detail"]["requestParameters"]["logGroupName"]
+    log_group_tags = event["detail"]["requestParameters"]["tags"]
   
     # Check whether the prefix is set - the prefix is used to determine which logs we want.
-    if not humio_subscription_prefix:
+    # Also check if there is a tag name specified - if the tag exists, then that filters logs as well.
+    if not humio_subscription_prefix and not humio_subscription_tag_name:
         helpers.create_subscription(
             log_client, log_group_name, humio_log_ingester_arn, context
         )
-
     else:
-        # Check whether the log group's name starts with the set prefix.
-        if log_group_name.startswith(humio_subscription_prefix):
+        # Are we filtering based on a prefix? If so, then check that the log group name is prefixed correctly.
+        matches_prefix = not humio_subscription_prefix or log_group_name.startswith(humio_subscription_prefix)
+
+        # Are we filtering on tags? If so, check for the EXISTENCE ONLY of the given tag.
+        has_tag_specified = not humio_subscription_tag_name or (humio_subscription_tag_name in log_group_tags.keys())
+
+        if has_tag_specified and matches_prefix:
             helpers.create_subscription(
+                log_client, log_group_name, humio_log_ingester_arn, context
+            )
+        else:
+            helpers.delete_subscription(
                 log_client, log_group_name, humio_log_ingester_arn, context
             )
